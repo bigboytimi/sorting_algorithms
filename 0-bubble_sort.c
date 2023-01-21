dbaa906a910cc925bdd471678573e7a689a3f885
bigboytimi <olutimiolowookere@icloud.com>
2023-01-21 18:15:10 +0000

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * swap - swap two values
+ *
+ * @xp: first value
+ * @yp: second value
+ *
+ * Return: Sorted value
+ */
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
+ * bubble_sort - algorithm to sort an unsorted array
+ *
+ * @array: an array of an unsorted array
+ * @size: size of the array
+ *
+ * Return: A sorted array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
