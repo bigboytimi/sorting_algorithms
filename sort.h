b6357187b36ee02a0638376ba4f1806155b7c743
bigboytimi <olutimiolowookere@icloud.com>
2023-01-23 19:39:47 +0000

    header files

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void shell_sort(int *array, size_t size);
 void quick_s(int *array, int lo, int hi, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
 void cocktail_sort_list(listint_t **list);
-
+int arr_max(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 #endif /* SORT_H */
