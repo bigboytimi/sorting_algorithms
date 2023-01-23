801cf8b855e472a830915dc93261065d39a051f2
bigboytimi <olutimiolowookere@icloud.com>
2023-01-23 19:50:29 +0000

    hotfix

--- sort.h
+++ sort.h
@@ -30,7 +30,7 @@ void cocktail_sort_list(listint_t **list);
 int arr_max(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void copy_array(int *arr, int 8cpy, int size);
+void copy_array(int *arr, int *cpy, int size);
 void mergeSort(int *cpy, int first, int size, int *array);
 void merge(int *array, int size, int first, int mid, int *cpy);
 void print_left_right(int *array, int size, int first, int mid);
